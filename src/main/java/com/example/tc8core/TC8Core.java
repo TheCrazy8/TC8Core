94094a2c43c90419eb3051b39d7178696ee90e6d
Scarlett/Eight <bennettbashford@outlook.com>
2026-07-09 17:12:49 -0400

    Add files via upload

--- src/main/java/com/example/tc8core/TC8Core.java
+++ src/main/java/com/example/tc8core/TC8Core.java
@@ -1,117 +1,108 @@
 package com.example.tc8core;
 
-import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(TC8Core.MODID)
 public class TC8Core {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "tc8core";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "tc8core" namespace
+
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "tc8core" namespace
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "tc8core" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new Block with the id "tc8core:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "tc8core:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
+    public static final DeferredItem<Item> CHISELING_TEMPLATE = ITEMS.register("chiseling_template", () -> new Item(new Item.Properties()));
 
-    // Creates a new food item with the id "tc8core:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+    public static final DeferredBlock<Block> CHISELED_LAPIS_BLOCK = registerBlock("chiseled_lapis_block", MapColor.LAPIS);
+    public static final DeferredBlock<Block> CHISELED_EMERALD_BLOCK = registerBlock("chiseled_emerald_block", MapColor.EMERALD);
+    public static final DeferredBlock<Block> CHISELED_GOLD_BLOCK = registerBlock("chiseled_gold_block", MapColor.GOLD);
+    public static final DeferredBlock<Block> CHISELED_COPPER_BLOCK = registerBlock("chiseled_copper_block", MapColor.COLOR_ORANGE);
+    public static final DeferredBlock<Block> CHISELED_IRON_BLOCK = registerBlock("chiseled_iron_block", MapColor.METAL);
+    public static final DeferredBlock<Block> CHISELED_NETHERITE_BLOCK = registerBlock("chiseled_netherite_block", MapColor.COLOR_BLACK);
+    public static final DeferredBlock<Block> CHISELED_AMETHYST_BLOCK = registerBlock("chiseled_amethyst_block", MapColor.COLOR_PURPLE);
+    public static final DeferredBlock<Block> CHISELED_DIAMOND_BLOCK = registerBlock("chiseled_diamond_block", MapColor.DIAMOND);
+    public static final DeferredBlock<Block> CHISELED_ZINC_BLOCK = registerBlock("chiseled_zinc_block", MapColor.METAL);
+    public static final DeferredBlock<Block> CHISELED_BRASS_BLOCK = registerBlock("chiseled_brass_block", MapColor.GOLD);
+    public static final DeferredBlock<Block> CHISELED_ANDESITE_ALLOY_BLOCK = registerBlock("chiseled_andesite_alloy_block", MapColor.METAL);
+    public static final DeferredBlock<Block> CHISELED_JADE_BLOCK = registerBlock("chiseled_jade_block", MapColor.EMERALD);
+    public static final DeferredBlock<Block> ORE_MOSAIC_BLOCK = registerBlock("ore_mosaic_block", MapColor.METAL);
 
-    // Creates a creative tab with the id "tc8core:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.tc8core")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+    public static final DeferredBlock<Block> CHISELED_REDSTONE_BLOCK = BLOCKS.register("chiseled_redstone_block", () ->
+            new PoweredChiseledBlock(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.COLOR_RED)
+                    .requiresCorrectToolForDrops()
+                    .strength(5.0F, 6.0F)));
+    public static final DeferredItem<BlockItem> CHISELED_REDSTONE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem(CHISELED_REDSTONE_BLOCK);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TC8_CORE_TAB = CREATIVE_MODE_TABS.register("tc8core", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.tc8core"))
+            .icon(() -> CHISELING_TEMPLATE.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+                output.accept(CHISELING_TEMPLATE.get());
+                output.accept(CHISELED_LAPIS_BLOCK.get());
+                output.accept(CHISELED_EMERALD_BLOCK.get());
+                output.accept(CHISELED_REDSTONE_BLOCK.get());
+                output.accept(CHISELED_GOLD_BLOCK.get());
+                output.accept(CHISELED_COPPER_BLOCK.get());
+                output.accept(CHISELED_IRON_BLOCK.get());
+                output.accept(CHISELED_NETHERITE_BLOCK.get());
+                output.accept(CHISELED_AMETHYST_BLOCK.get());
+                output.accept(CHISELED_DIAMOND_BLOCK.get());
+                output.accept(CHISELED_ZINC_BLOCK.get());
+                output.accept(CHISELED_BRASS_BLOCK.get());
+                output.accept(CHISELED_ANDESITE_ALLOY_BLOCK.get());
+                output.accept(CHISELED_JADE_BLOCK.get());
+                output.accept(ORE_MOSAIC_BLOCK.get());
             }).build());
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public TC8Core(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
         CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (TC8Core) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
+    private static DeferredBlock<Block> registerBlock(String name, MapColor color) {
+        DeferredBlock<Block> block = BLOCKS.register(name, () -> new Block(BlockBehaviour.Properties.of()
+                .mapColor(color)
+                .requiresCorrectToolForDrops()
+                .strength(5.0F, 6.0F)));
+        ITEMS.registerSimpleBlockItem(block);
+        return block;
+    }
 
-        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
-            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+    public static class PoweredChiseledBlock extends Block {
+        public PoweredChiseledBlock(Properties properties) {
+            super(properties);
         }
 
-        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
-
-        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        @Override
+        public boolean isSignalSource(BlockState state) {
+            return true;
         }
-    }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        @Override
+        public int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+            return 15;
+        }
     }
 }
